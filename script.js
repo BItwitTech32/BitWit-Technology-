f350dbca33b8b3a3dfd4a659d3b8d339cd86ab45
BItwitTech32 <bitwittechnologies@gmail.com>
2025-03-29 15:18:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("Bitwit Technologies Website Loaded!");
-
-    const services = document.querySelectorAll(".service");
-    services.forEach(service => {
-        service.addEventListener("mouseover", () => {
-            service.style.transform = "rotate(3deg)";
-        });
-        service.addEventListener("mouseout", () => {
-            service.style.transform = "rotate(0deg)";
-        });
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("BitWit Technologies Website Loaded");
+    
+    // Live chat integration (Example using Tawk.to)
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/YOUR_TAWK_ID/default';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+    })();
 });
